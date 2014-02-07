1356feec8f4c71071dd6a99a30b78506c9a953eb
julong <julongdragon@gmail.com>
2014-02-07 14:36:52 +0700

    touch event

--- learn-corona/main.lua
+++ learn-corona/main.lua
@@ -15,18 +15,18 @@ _W = display.viewableContentWidth
 _H = display.viewableContentHeight
 
 -- platformVersion
-local platform = getInfo( "platformVersion" );
+local platform = system.getInfo( "platformVersion" );
 local font = (platform ~= "Android") and "Helvetica-Light" or system.nativeFont;
 
 --font  // http://iosfonts.com
 local foo = display.newText( "Hello", 0 , 0 , font , 32);
-font:setTextColor( 0, 0, 0 );
-font:setReferencePoint( CenterReferencePoint );
-font.x = _W * 0.5;
-font.y = _H * 0.5;
+foo:setTextColor( 89,89,89);
+foo.anchorX = 0;
+foo.x = _W * 0.5;
+foo.y = _H * 0.5;
 
 --Background  (white color)
-local background_color = newRect( 0, 0, 320, 480 );
+local background_color = display.newRect( 0, 0, 320, 480 );
 background_color:setFillColor( 255 , 255 , 255 );
 
 --Image
@@ -43,22 +43,26 @@ local function loadImage(e)
 		transition.to( reloadImage, {alpha = 1} );
 	end
 end
-      --loadRemoteImage( url, method, listener [, params], destFilename [, baseDir] [, x, y] )
-display.loadRemoteImage( "http://theme5hq.com/wp-content/uploads/2014/01/itune-01.jpg", "GET", loadImage , "temp" ,system.TemporaryDirectory ,0,0);
+--loadRemoteImage( url, method, listener [, params], destFilename [, baseDir] [, x, y] )
+--[[local loadimage_fromUrl = display.loadRemoteImage( "http://theme5hq.com/wp-content/uploads/2014/01/itune-01.jpg", "GET", loadImage , "temp" ,system.TemporaryDirectory ,0,0);
+loadimage_fromUrl = removeSelf;
+loadimage_fromUrl = nil;
+]]
 
 --Group
-local group1 = display.newGroup( );
+--[[
+local g1 = display.newGroup( );
 local bar = display.newText("bar",0,0,system.nativefont,32);
-bar:setTextColor( 0, 0, 0  );
-group1.insert(reloadImage);
-group1.insert(bar);
-reloadImage.x = _W * 0.5;
-reloadImage.y = _H * 0.5;
-bar.x = reloadImage.x;
-bar.y = reloadImage.y;
+bar:setTextColor( 0, 0, 0);
+g1.insert(loadimage_fromUrl)
+g1:insert(bar)
+loadimage_fromUrl.x = _W * 0.5;
+loadimage_fromUrl.y = _H * 0.5;
+bar.x = loadimage_fromUrl.x;
+bar.y = loadimage_fromUrl.y;
 group1.x = _W * 0.5;
 group1.y = _H * 0.5;
-
+]]
 -- background green with Mask
 local mask = graphics.newMask("assets/img/mask.png");
 local bg_green = display.newImageRect( "assets/img/bg-green.png", 320, 480 );
@@ -67,10 +71,10 @@ bg_green.y = _H * 0.5;
 bg_green:setMask( mask );
 
 -- getinfo texture memory
-local texturememory_used = system.getInfo( "texturememory" );
+local texturememory_used = system.getInfo( "textureMemoryUsed" );
 texturememory_used = texturememory_used/1024/1024 ;
 print("texture memory used : " .. texturememory_used);
-local Maxtexture = system.getInfo( "Maxtexture" );
+local Maxtexture = system.getInfo( "maxTextureSize" );
 print("Maxtexture memory : " .. Maxtexture);
 
 --Release Memory
@@ -86,19 +90,25 @@ bar = nil;
 --local screenShort = display.captureScreen(true);
 
 --addEventListener
-
-function bar:touch(e)
-	if (e.phase == "began")then
+local bar = display.newText("EAKKASIT",200,100,system.nativefont,32);
+function bar:touch(event)
+	if (event.phase == "began")then
+		display.getCurrentStage():setFocus( self )
+        self.isFocus = true
 		print("touch began");
-	elseif (e.phase == "moved")then
+	elseif self.isFocus  then
+		if(event.phase == "moved") then
 		print("touch moved");
-	elseif (e,phase == "ended") or e,phase == "cancled" then
+		elseif (event.phase == "ended") or (event.phase == "cancled") then
+		display.getCurrentStage():setFocus( nil )
+        self.isFocus = false
 		print("touch ended");
+		end
 	end
 	return true;
 end
 
-addEventListener("touch",bar);
+bar:addEventListener("touch",bar);
 
 
 
